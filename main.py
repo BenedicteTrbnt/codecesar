bcb82e53bc9262030ac43d81334062e2f53801ba
beneturbant <benedicte.turbant.1@ens.etsmtl.ca>
2024-05-22 15:21:32 -0400

    codage (fonctionnel) d'une chaine de caratères avec une clé positive ou négative (avec correction des variables

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import string
 alphabet=string.ascii_lowercase
-print(alphabet)
-print(alphabet.find('z'))
-clé=int(input('Quel est la clé de décodage, entrez un entier positif ou négatif: '))
+#print(alphabet)
+#print(alphabet.find('z'))
+message_a_coder=str(input('Entez la chaine de caractères à coder: '))
+#encodage d'une chaîne de caratères rentrée par l'utilisateur
+clé=int(input('Quel est la clé de codage, entrez un entier positif ou négatif: '))
+#encodage d'une chaîne de caratères rentrée par l'utilisateur
 message_a_coder=str(input('Entez la chaine de caractères à décoder: '))
 longueur_message=len(message_a_coder)
-print(longueur_message)
-print(message_a_coder)
+#print(longueur_message)
+#print(message_a_coder)
 message_codé=''
 for i in range(longueur_message):
     indice=alphabet.find(message_a_coder[i])
