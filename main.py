50214d1eeb39e5dd96aebc6c67657d5fcd4adaa3
beneturbant <benedicte.turbant.1@ens.etsmtl.ca>
2024-05-22 15:53:36 -0400

    Script pour le codage d'une chaîne de caractères entrée par l'utilisateur avec une clé positive ou négative (fonctionne des chaînes de caractères contenant des espaces et pour l'instant que pour des lettres minuscules)

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import string
 alphabet=string.ascii_lowercase
 ALPHABET=string.ascii_uppercase
-#print(alphabet)
-#print(alphabet.find('z'))
 message_a_coder=str(input('Entez la chaine de caractères à coder: '))
 print(message_a_coder)
 #encodage d'une chaîne de caratères rentrée par l'utilisateur
 clé=int(input('Quel est la clé de codage, entrez un entier positif ou négatif: '))
 longueur_message=len(message_a_coder)
-#print(longueur_message)
-#print(message_a_coder)
 message_codé=''
 for i in range(longueur_message):
     indice=alphabet.find(message_a_coder[i])
